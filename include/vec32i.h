9ef92bc296edc8f95d0b3337f1750dfb8f8d1476
Yanis Fourel <yanis.fourel@ledr.io>
2024-03-31 10:34:34 +0800

    Now use clang-format

--- include/vec32i.h
+++ include/vec32i.h
@@ -1,24 +1,24 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <sys/types.h>
-#include <stdbool.h>
 
 typedef struct s_vec32i {
-	int32_t *data;
-	u_int32_t size;
-	u_int32_t _cap;
+    int32_t *data;
+    u_int32_t size;
+    u_int32_t _cap;
 } t_vec32i;
 
-
-#define vec32i_from(...) vec32i_from_buff((int32_t const*)(int32_t []) __VA_ARGS__, sizeof((int32_t []) __VA_ARGS__)/sizeof(int32_t))
-
+#define vec32i_from(...)                                                       \
+    vec32i_from_buff((int32_t const *)(int32_t[])__VA_ARGS__,                  \
+                     sizeof((int32_t[])__VA_ARGS__) / sizeof(int32_t))
 
 t_vec32i *vec32i_new(u_int32_t capacity);
-t_vec32i *vec32i_from_buff(int32_t const* buff, size_t size);
+t_vec32i *vec32i_from_buff(int32_t const *buff, size_t size);
 void vec32i_free(t_vec32i *vec);
 t_vec32i *vec32i_append(t_vec32i *vec, int32_t n);
 void vec32i_remove(t_vec32i *vec, size_t index);
 void vec32i_print(t_vec32i *vec);
-bool vec32i_eq(t_vec32i const* a, t_vec32i const *b);
+bool vec32i_eq(t_vec32i const *a, t_vec32i const *b);
--- src/vec32i.c
+++ src/vec32i.c
@@ -4,81 +4,82 @@
 #include <string.h>
 
 t_vec32i *vec32i_new(u_int32_t capacity) {
-	t_vec32i *res = malloc(sizeof(t_vec32i));
-	if (res == NULL) {
-		return NULL;
-	}
+    t_vec32i *res = malloc(sizeof(t_vec32i));
+    if (res == NULL) {
+        return NULL;
+    }
 
-	if (capacity < 8) {
-		capacity = 8;
-	}
-	res->data = malloc(capacity * sizeof(int32_t));
-	if (res->data == NULL) {
-		free(res);
-		return NULL;
-	}
+    if (capacity < 8) {
+        capacity = 8;
+    }
+    res->data = malloc(capacity * sizeof(int32_t));
+    if (res->data == NULL) {
+        free(res);
+        return NULL;
+    }
 
-	res->_cap = capacity;
-	res->size = 0;
-	return res;
+    res->_cap = capacity;
+    res->size = 0;
+    return res;
 }
 
-t_vec32i *vec32i_from_buff(int32_t const* buff, size_t size) {
-	t_vec32i *res = vec32i_new(size);
-	if (res == NULL) {
-		return NULL;
-	}
-	memcpy(res->data, buff, size);
-	res->size = size;
-	return res;
+t_vec32i *vec32i_from_buff(int32_t const *buff, size_t size) {
+    t_vec32i *res = vec32i_new(size);
+    if (res == NULL) {
+        return NULL;
+    }
+    memcpy(res->data, buff, size);
+    res->size = size;
+    return res;
 }
 
 void vec32i_free(t_vec32i *vec) {
-	free(vec->data);
-	free(vec);
+    free(vec->data);
+    free(vec);
 }
 
 // This potentially reallocates the data field, don't keep any reference to the
 // old pointer
 t_vec32i *vec32i_append(t_vec32i *vec, int32_t n) {
-	if (vec->size + 1 > vec->_cap) {
-		vec->_cap *= 2;
-		vec->data = realloc(vec->data, vec->_cap * sizeof(int32_t));
-		if (vec->data == NULL) {
-			return NULL;
-		}
-	}
+    if (vec->size + 1 > vec->_cap) {
+        vec->_cap *= 2;
+        vec->data = realloc(vec->data, vec->_cap * sizeof(int32_t));
+        if (vec->data == NULL) {
+            return NULL;
+        }
+    }
 
-	vec->data[vec->size] = n;
-	vec->size += 1;
-	return vec;
+    vec->data[vec->size] = n;
+    vec->size += 1;
+    return vec;
 }
 
 void vec32i_remove(t_vec32i *vec, size_t index) {
-	if (index >= vec->size) {
-		fprintf(stderr, "Out of bound removal of index [%zu] on size %u\n", index, vec->size);
-		exit(EXIT_FAILURE);
-	}
-	for (size_t i = index; i < vec->size; i++) {
-		vec->data[i] = vec->data[i+1];
-	}
-	vec->size -= 1;
+    if (index >= vec->size) {
+        fprintf(stderr, "Out of bound removal of index [%zu] on size %u\n",
+                index, vec->size);
+        exit(EXIT_FAILURE);
+    }
+    for (size_t i = index; i < vec->size; i++) {
+        vec->data[i] = vec->data[i + 1];
+    }
+    vec->size -= 1;
 }
 
-bool vec32i_eq(t_vec32i const* a, t_vec32i const *b) {
-	if (a->size != b->size) {
-		return false;
-	}
-	return memcmp(a->data, b->data, a->size) == 0;
+bool vec32i_eq(t_vec32i const *a, t_vec32i const *b) {
+    if (a->size != b->size) {
+        return false;
+    }
+    return memcmp(a->data, b->data, a->size) == 0;
 }
 
 void vec32i_print(t_vec32i *vec) {
-	printf("<vec32i size=%3u, cap=%3u: [", vec->size, vec->_cap);
-	for (size_t i = 0; i < vec->size; i++) {
-		if (i != 0) {
-			printf(", ");
-		}
-		printf("%d", vec->data[i]);
-	}
-	printf("]>\n");
+    printf("<vec32i size=%3u, cap=%3u: [", vec->size, vec->_cap);
+    for (size_t i = 0; i < vec->size; i++) {
+        if (i != 0) {
+            printf(", ");
+        }
+        printf("%d", vec->data[i]);
+    }
+    printf("]>\n");
 }
