b087f364e104d3e3cc44a3a194e0693844bd27f9
Yanis Fourel <yanis.fourel42@proton.me>
2025-03-22 15:46:17 +0800

    bettr myarraylist

--- zig/src/myarraylist.zig
+++ zig/src/myarraylist.zig
@@ -1,47 +1,63 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const print = std.debug.print;
+const assert = std.debug.assert;
 
 /// Contiguous growable memory buffer
-pub fn MyArrayList(comptime t: type) type {
+pub fn MyArrayList(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        items: []t,
-        size: u32,
+        comptime {
+            assert(@sizeOf(T) != 0);
+        }
+
+        items: []T,
+        capacity: usize,
         alloc: Allocator,
 
         pub fn init(alloc: Allocator) !Self {
             return Self{
-                .items = try alloc.alloc(t, 8),
-                .size = 0,
+                .capacity = 0,
+                .items = &[_]T{},
                 .alloc = alloc,
             };
         }
 
         pub fn deinit(self: *Self) void {
+            self.items.len = self.capacity;
             self.alloc.free(self.items);
         }
 
-        pub fn push(self: *Self, val: t) !void {
-            if (self.items.len == self.size) {
+        pub fn append(self: *Self, val: T) !void {
+            if (self.items.len + 1 >= self.capacity) {
                 try self.grow();
             }
-            self.items[self.size] = val;
-            self.size += 1;
+            self.items.len += 1;
+            self.items[self.items.len - 1] = val;
         }
 
-        pub fn pop(self: *Self) ?t {
-            if (self.size == 0) {
+        pub fn pop(self: *Self) ?T {
+            if (self.items.len == 0) {
                 return null;
             }
-            self.size -= 1;
-            return self.items[self.size];
+            const res = self.items[self.items.len - 1];
+            self.items.len -= 1;
+            return res;
         }
 
         pub fn grow(self: *Self) !void {
-            const newlen = self.size * 2;
-            self.items = try self.alloc.realloc(self.items, newlen);
-            self.size = newlen;
+            const newlen = (self.capacity + 1) * 2;
+            const newslice = try self.alloc.alloc(T, newlen);
+
+            @memcpy(newslice[0..self.items.len], self.items);
+            self.alloc.free(self.items.ptr[0..self.capacity]);
+            self.items.ptr = newslice.ptr;
+            self.capacity = newslice.len;
+        }
+
+        pub fn empty(self: Self) bool {
+            return self.items.len == 0;
         }
     };
 }
@@ -54,7 +70,12 @@ test "MyArrayList" {
     defer al.deinit();
 
     try expect(al.pop() == null);
-    try al.push(42);
+    try al.append(42);
     try expect(al.pop() == 42);
     try expect(al.pop() == null);
+
+    var i: u8 = 0;
+    while (i < 100) : (i += 1) {
+        try al.append(i);
+    }
 }
